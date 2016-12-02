bc8d1e225362c87f0930cb4ffdf97b56e1f19699
anugool tippon <anugooltippon@gmail.com>
2016-12-02 11:48:48 +0700

    register

--- register.php
+++ register.php
@@ -1,6 +1,8 @@
 <?php include('header.php');?>
 <?php include('topheader.php');?>
 
+
+
 <div class="block">
   <div class="container">
     <ul class="breadcrumbs">
@@ -16,36 +18,70 @@
 <div class="block boxed">
   <div class="container">
     <div class="form-card">
-      <h3>สมัครเลย</h3>
-      <br/>
-      <form method="post" action="#" id="create_customer" class="account-create" accept-charset="UTF-8">
-        <input type="hidden" value="create_customer" name="form_type" /><input type="hidden" name="utf8" value="✓" />
-        <div class="col-sm-6">
-        <label>ชื่อ<span class="required">*</span></label>
-        <input type="text" class="form-control input-lg" name="customer[first_name]" id="FirstName"  autocapitalize="words" autofocus>
-        </div>
+      <h2>Register<p class="lead">Name - last name as identification. To insist on auction</p></h2>
+            <form role="form" class="form-horizontal">
+              <div class="form-group">
+                  <div class="col-sm-3"><label>First name</label><input type="text" class="form-control" placeholder="First"></div>
+                  <div class="col-sm-3"><label>Last name</label><input type="text" class="form-control" placeholder="Last"></div>
+              </div>
 
-      <div class="col-sm-6">
-        <label>นามสกุล<span class="required">*</span></label>
-        <input class="form-control input-lg" type="text" name="customer[last_name]" id="LastName"  autocapitalize="words">
-      </div>
 
-        <label>Email<span class="required">*</span></label>
-        <input class="form-control input-lg" type="email" name="customer[email]" id="Email"  autocorrect="off" autocapitalize="off">
+              <div class="form-group">
+                <div class="col-sm-3"><label>Birthday</label><input type="text" class="form-control" placeholder="01/02/2534"></div>
+                <div class="col-sm-3"><label>Gender</label><input type="text" class="form-control" placeholder="Male & Female"></div>
+              </div>
 
-        <label>Password<span class="required">*</span></label>
-        <input class="form-control input-lg" type="password" name="customer[password]" id="CreatePassword" >
+              <div class="form-group">
+                  <label class="col-sm-12" for="Phone">Phone</label>
+                  <div class="col-sm-6"><input type="text" class="form-control"  placeholder="Phone Number"></textarea></div>
+              </div>
 
-        <div><button class="btn">สมัครเลย</button><span class="required-text">* Required Fields</span></div>
-      </form>
+              <div class="form-group">
+                  <label class="col-sm-12" for="TextArea">Address</label>
+                  <div class="col-sm-6"><textarea class="form-control" id="TextArea"></textarea></div>
+              </div>
 
+              <div class="form-group">
+                  <label class="col-sm-1" for="Province">Province</label>
+                  <div class="col-sm-5"><input type="province" class="form-control" id="province" placeholder="Province"></div>
+              </div>
+
+              <div class="form-group">
+                  <label class="col-sm-1" for="inputEmail1">Zipcode</label>
+                  <div class="col-sm-5"><input type="Zipcode" class="form-control" id="Zipcode" placeholder="Zipcode"></div>
+              </div>
+
+              <h2>Please<p class="lead">Data logging</p></h2>
+              <div class="form-group">
+                  <label class="col-sm-1" for="inputEmail1">Username</label>
+                  <div class="col-sm-5"><input type="username" class="form-control" id="username" placeholder="username"></div>
+              </div>
+                <div class="form-group">
+                    <label class="col-sm-1" for="inputEmail1">Email</label>
+                    <div class="col-sm-5"><input type="email" class="form-control" id="inputEmail1" placeholder="Email"></div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-1" for="inputPassword1">Password</label>
+                    <div class="col-sm-5"><input type="password" class="form-control" id="inputPassword1" placeholder="Password"></div>
+                </div>
+
+
+                <div class="form-group">
+                    <div class="col-sm-6">
+                        <button type="submit" class="btn btn-info pull-right">Submit</button>
+                    </div>
+                </div>
+          </form>
     </div>
+
+
   </div>
 </div>
+
+
+
+
+
 </main>
-
-
-
-
 
 <?php include('footer.php');?>
